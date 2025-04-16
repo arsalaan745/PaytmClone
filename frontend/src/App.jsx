b07ab6de7f68e85c38ef38f40bfc3e480f5eaaaf
Arsalaan Ahmad <arsalaan745@gmail.com>
2025-04-16 16:01:59 +0530

    Added routes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Router, Route } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/signup" element={<Signup/>} />
           <Route path="/signin" element={<Signin/>} />
           <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/sendmoney" element={<SendMoney/>} />
         </Routes>
       </BrowserRouter>
     </>
